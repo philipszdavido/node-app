af28280b1b9090849cc3de7372bd154851de63fd
Chidume Nnamdi <kurtwanger5@gmail.com>
2020-05-18 13:55:52 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-
 /** require dependencies */
 const express = require("express")
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
+const path = require('path')
 
 const app = express()
 
-/** important */
 let port = process.env.PORT || 5000
+
+/** set up middlewares */
+app.use(cors())
+app.use(bodyParser.json({limit: '50mb'}))
+app.use(helmet())
 
 app.get("/", (request, response) => {
     response.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 /** start server */
