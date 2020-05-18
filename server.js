7ceedac5e814afb1bad35ed16b876489625859e3
Chidume Nnamdi <kurtwanger5@gmail.com>
2020-05-18 13:28:32 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+
+/** require dependencies */
+const express = require("express")
+
+const app = express()
+
+/** !important */
+let port = process.env.PORT || 5000
+
+app.get("/", (request, response) => {
+    response.sendFile(path.join(__dirname, 'index.html'));
+});
+
+/** start server */
+app.listen(port, () => {
+    console.log(`Server started at port: ${port}`);
+});
